b4a9eee84a29350f6007e4f321a44c488609b5c7
Adam Longman <boardhead89@gmail.com>
2020-01-02 19:45:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,6 @@ end
 
 def print_first_directors_movie_titles
   first_director = directors_database.reduce(nil) |memo|
-  memo = directors_database[0] if !memo
+  memo = directors_database[0]
   p first_director
 end
